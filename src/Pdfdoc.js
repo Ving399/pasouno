8bddd450fe863b9bb6cd4a6ca45bd1421a35543c
root <root@Wembley.>
2026-03-16 16:26:52 -0600

    mejoras pdf

--- src/Pdfdoc.js
+++ src/Pdfdoc.js
@@ -1,19 +1,40 @@
 import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 import logo from './assets/icons/rebels_icon.png';
+import logoPasouno from './assets/icons/pasounof.png';
+import imgMaya from './assets/icons/maya.png';
+import imgOtomi from './assets/icons/otomi.png';
+import imgWixarica from './assets/icons/wixarica.png';
+import imgKikapu from './assets/icons/kikapu.png';
+
+const iconosRuta = {
+    maya:     imgMaya,
+    otomi:    imgOtomi,
+    wixarica: imgWixarica,
+    kikapu:   imgKikapu,
+};
+
+const descripcionesRuta = {
+    maya:     'Medio ambiente y sostenibilidad',
+    otomi:    'Habilidades para la vida',
+    wixarica: 'Paz y participación comunitaria',
+    kikapu:   'Salud y bienestar',
+};
 
 const colores = {
     negro:      '#111111',
     grisOscuro: '#444444',
     grisMedio:  '#222222',
     blanco:     '#ffffff',
-    cajaMorada: '#8989ef',
+    cajaMorada: '#e0e0ff',
     badgeVerde: '#4ade80',
 };
 
 const s = StyleSheet.create({
     page: {
-        padding: 40,
         backgroundColor: colores.blanco,
+    },
+    contenido: {
+        padding: 40,
     },
     header: {
         flexDirection: 'row',
@@ -29,6 +50,17 @@ const s = StyleSheet.create({
     logo: {
         width: 50,
         height: 50,
+    },
+    navbar: {
+        backgroundColor: '#7f60ed',
+        padding: 12,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    navbarLogo: {
+        width: 60,
+        height: 24,
     },
     rover: {
         fontSize: 12,
@@ -83,10 +115,16 @@ function PDFDoc({ proyecto, rover, ruta, objetivoGeneral, objetivos, ods1, ods2,
         <Document>
             <Page style={s.page}>
 
+                {/* Navbar */}
+                <View style={s.navbar}>
+                    <Image style={s.navbarLogo} src={logoPasouno} />
+                </View>
+
+                <View style={s.contenido}>
                 {/* Header: nombre del proyecto + logo */}
                 <View style={s.header}>
                     <Text style={s.proyecto}>{proyecto}</Text>
-                    <Image style={s.logo} src={logo} />
+                    
                 </View>
 
                 {/* Rover */}
@@ -95,8 +133,11 @@ function PDFDoc({ proyecto, rover, ruta, objetivoGeneral, objetivos, ods1, ods2,
                 {/* Ruta badge */}
                 <View style={s.rutaRow}>
                     <Text style={s.rutaLabel}>Ruta</Text>
+                    {iconosRuta[ruta] && (
+                        <Image style={{ width: 40, height: 40 }} src={iconosRuta[ruta]} />
+                    )}
                     <View style={s.rutaBadge}>
-                        <Text style={s.rutaBadgeText}>{ruta}</Text>
+                        <Text style={s.rutaBadgeText}>{descripcionesRuta[ruta]}</Text>
                     </View>
                 </View>
 
@@ -121,6 +162,7 @@ function PDFDoc({ proyecto, rover, ruta, objetivoGeneral, objetivos, ods1, ods2,
                         <Text style={s.cajaTexto}>{ods}</Text>
                     </View>
                 ))}
+                </View>
 
             </Page>
         </Document>
